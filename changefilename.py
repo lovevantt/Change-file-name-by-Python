6a9c36941abe6fd6e0c74612021516e6cec059dd
lovevantt <lovevantt@gmail.com>
2019-12-11 20:16:16 +0800

    realize the function of not closing the program after running

--- changefilename.py
+++ changefilename.py
@@ -19,14 +19,15 @@ for f in files:
         midfile=os.path.join(mypath,file_split[0])
         os.rename(oldfile,midfile)
         oldfile=midfile
-        f=file_split[0]#如果有这句的话就会出现 1.aaa.bbb 虽然后缀为.bbb，但是仍然会被命名为1.bbb。
+        f=file_split[0]
     if f.endswith(suffixe):
         print(f+"无需重命名")
         pass
     else:
-        f=file_split[0]
+        f=file_split[0]#如果没有这句的话就会出现 1.aaa 命名后缀为.bbb，就会被命名为1.aaa.bbb。
         newf=f+suffixe
         newfile=os.path.join(mypath,newf)
         os.rename(oldfile,newfile)
-        print(newf+"重命名已完成")
+        print(newf+"重命名完成")
 
+os.system('pause')
