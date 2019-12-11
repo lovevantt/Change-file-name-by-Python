e4cc5398c75ac91955bd79d42d63f0df8b5891f9
lovevantt <lovevantt@gmail.com>
2019-12-11 10:55:57 +0800

    修复了1.aaa会重命名为1.aaa.bbb的bug，但是现在1.aaa.bbb会被重命名为1.bbb，虽然他们后缀一样。

--- changefilename.py
+++ changefilename.py
@@ -15,15 +15,16 @@ suffixe="."+suf
 for f in files:
     oldfile=os.path.join(mypath,f)
     file_split=f.split('.')
-    if file_split[2]:
+    if len(file_split)>2:#if file_split[2]:
         midfile=os.path.join(mypath,file_split[0])
         os.rename(oldfile,midfile)
         oldfile=midfile
-        f=file_split[0]
+        f=file_split[0]#如果有这句的话就会出现 1.aaa.bbb 虽然后缀为.bbb，但是仍然会被命名为1.bbb。
     if f.endswith(suffixe):
         print(f+"无需重命名")
         pass
     else:
+        f=file_split[0]
         newf=f+suffixe
         newfile=os.path.join(mypath,newf)
         os.rename(oldfile,newfile)
